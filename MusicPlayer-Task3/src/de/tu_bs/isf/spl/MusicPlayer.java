c9a359c7de7191bd3df318570f727661c03e97e6
Phillipp Mevenkamp <phillippmevenkamp@gmail.com>
2018-05-21 13:18:52 +0200

    Add preprocessor directives to code

--- MusicPlayer-Task3/src/de/tu_bs/isf/spl/MusicPlayer.java
+++ MusicPlayer-Task3/src/de/tu_bs/isf/spl/MusicPlayer.java
@@ -1,39 +1,46 @@
 package de.tu_bs.isf.spl;
 
+import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
+import org.jfugue.theory.ChordProgression;
 
 import de.tu_bs.isf.spl.instrument.IInstrument;
 import de.tu_bs.isf.spl.instrument.Piano;
 import de.tu_bs.isf.spl.instrument.Violin;
 import de.tu_bs.isf.spl.melody.IMelodyProducer;
 import de.tu_bs.isf.spl.melody.PredefinedProducer;
-import properties.PropertyManager;
 
 public class MusicPlayer {
 
 	public static void main(String[] args) {		
+		//#ifdef Piano
 		IInstrument instrument = new Piano();
-		
-		if(PropertyManager.getProperty("Piano")) {
-			instrument = new Piano();
-		}
-		if(PropertyManager.getProperty("Violin")) {
-			instrument = new Violin();
-		}
+		//#endif
+		//#ifdef Violin
+//@		IInstrument instrument = new Violin();
+		//#endif
 		
 		IMelodyProducer melody = new PredefinedProducer();
 		
-		String pattern = createPattern(instrument, melody);
+		Pattern pattern = createPattern(instrument, melody);
 		
 		Player player = new Player();
 		player.play(pattern);
 	}
 	
-	private static String createPattern(IInstrument instrument, IMelodyProducer producer) {
+	private static Pattern createPattern(IInstrument instrument, IMelodyProducer producer) {
 		final String instrumentPattern = "V0 I[" + instrument.getPattern() + "] ";
 	
+		final Pattern melody = new Pattern(instrumentPattern + producer.getMelodyPattern()); 
 		
-		return instrumentPattern + producer.getMelodyPattern();
+		//#ifdef Andante
+//@		melody.setTempo(70);
+		//#endif
+		//#ifdef Allegro
+		melody.setTempo(120);
+		//#endif
+		
+		return melody;
 	}
 
 }
