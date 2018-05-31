e658270c1ba2209a8ea440f4fb6dbf4a2381953e
Phillipp Mevenkamp <phillippmevenkamp@gmail.com>
2018-05-31 13:03:19 +0200

    Make piano a plugin

--- MusicPlayer-Task4/src/de/tu_bs/isf/spl/MusicPlayer.java
+++ MusicPlayer-Task4/src/de/tu_bs/isf/spl/MusicPlayer.java
@@ -1,27 +1,31 @@
 package de.tu_bs.isf.spl;
 
+import java.util.List;
+
 import org.jfugue.pattern.Pattern;
 import org.jfugue.player.Player;
 import org.jfugue.theory.ChordProgression;
 
 import de.tu_bs.isf.spl.instrument.IInstrument;
-import de.tu_bs.isf.spl.instrument.Piano;
 import de.tu_bs.isf.spl.instrument.Violin;
 import de.tu_bs.isf.spl.melody.IMelodyProducer;
 import de.tu_bs.isf.spl.melody.PredefinedProducer;
+import loader.PluginLoader;
 
 public class MusicPlayer {
 
 	public static void main(String[] args) {		
-		IInstrument instrument = new Piano();
+		List<IInstrument> instruments = PluginLoader.load(IInstrument.class);
+		System.out.println(instruments);
+		
 //@		IInstrument instrument = new Violin();
 		
 		IMelodyProducer melody = new PredefinedProducer();
 		
-		Pattern pattern = createPattern(instrument, melody);
+		//Pattern pattern = createPattern(instrument, melody);
 		
 		Player player = new Player();
-		player.play(pattern);
+		//player.play(pattern);
 	}
 	
 	private static Pattern createPattern(IInstrument instrument, IMelodyProducer producer) {
